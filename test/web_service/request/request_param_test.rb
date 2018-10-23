296c0efe7e4c6b116d028c9ce0d7944c6d3edf4d
Michael Harrison <michael@ntechmedia.com>
2018-10-24 09:42:03 +1000

    Tidied up XML to make the test more readable

--- test/web_service/request/request_param_test.rb
+++ test/web_service/request/request_param_test.rb
@@ -11,16 +11,24 @@ module WebService
         param = Exlibris::Primo::WebService::Request::RequestParam.new()
         param.key = @key
         param.value = @value
-        assert_equal "<RequestParam key=\"#{@key}\">" \
-                     "#{@value}" \
-                     "</RequestParam>", param.to_xml
+        expected_xml = strip_xml(
+          <<-XML
+          <RequestParam key="#{@key}">#{@value}</RequestParam>
+        XML
+        )
+
+        assert_equal expected_xml, param.to_xml
       end
 
       def test_write_attributes
         param = Exlibris::Primo::WebService::Request::RequestParam.new(:key => 'pc_availability_ind', :value => 'true')
-        assert_equal "<RequestParam key=\"#{@key}\">" \
-                     "#{@value}" \
-                     "</RequestParam>", param.to_xml
+        expected_xml = strip_xml(
+          <<-XML
+          <RequestParam key="#{@key}">#{@value}</RequestParam>
+        XML
+        )
+
+        assert_equal expected_xml, param.to_xml
       end
     end
   end
