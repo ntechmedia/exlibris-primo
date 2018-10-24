74949125be9d210fce9669d8185c91560860128b
Michael Harrison <michael@ntechmedia.com>
2018-10-24 14:08:07 +1000

    Added Exlibris::Primo::WebService::Request::Location.to_s method

--- lib/exlibris/primo/web_service/request/search/location.rb
+++ lib/exlibris/primo/web_service/request/search/location.rb
@@ -2,12 +2,19 @@ module Exlibris
   module Primo
     module WebService
       module Request
-        # 
-        # 
-        # 
+        #
+        #
+        #
         class Location
           include WriteAttributes
           attr_accessor :kind, :value
+
+          # Returns a string for inclusion as a query parameter for the REST API
+          def to_s
+            return '' unless kind == 'local'
+
+            "scope=#{value.gsub(/scope:|\(|\)/, '')}"
+          end
         end
       end
     end
--- test/web_service/request/location_test.rb
+++ test/web_service/request/location_test.rb
@@ -8,7 +8,7 @@ module WebService
       end
 
       def test_set_attributes
-        location = Exlibris::Primo::WebService::Request::Location.new()
+        location = Exlibris::Primo::WebService::Request::Location.new
         location.kind = @kind
         location.value = @value
         assert_equal "local", location.kind
@@ -19,6 +19,13 @@ module WebService
         location = Exlibris::Primo::WebService::Request::Location.new(:kind => @kind, :value => @value)
         assert_equal "local", location.kind
         assert_equal "scope:(VOLCANO)", location.value
+      end
+
+      def test_to_s
+        location = Exlibris::Primo::WebService::Request::Location.new(:kind => @kind, :value => @value)
+        expected_output = 'scope=VOLCANO'
+
+        assert_equal expected_output, location.to_s
       end
     end
   end
