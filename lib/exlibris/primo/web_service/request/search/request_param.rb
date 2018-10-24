c3ac24418ec188ff563f03b58ed8227425a8f404
Michael Harrison <michael@ntechmedia.com>
2018-10-24 10:02:04 +1000

    Added Exlibris::Primo::WebService::Request::RequestParam.to_s method

--- lib/exlibris/primo/web_service/request/search/request_param.rb
+++ lib/exlibris/primo/web_service/request/search/request_param.rb
@@ -17,6 +17,26 @@ module Exlibris
               }
             end
           end
+
+          # Returns a string for inclusion in the as a query parameter for the REST API
+          def to_s
+            if accepted.keys.exclude? key
+              raise "The provided request parameter key #{key} is unknown. " \
+                    "Please provide one of the following: #{accepted.keys.join(',')}"
+            end
+            if accepted[key].exclude? value
+              raise "The provided request parameter value #{value} is unacceptable. " \
+                    "Please provide one of the following: #{accepted[key].join(',')}"
+            end
+
+            "#{key}=#{value}"
+          end
+
+          def accepted
+            {
+              'pcAvailability' => %w(true false)
+            }
+          end
         end
       end
     end
--- test/web_service/request/request_param_test.rb
+++ test/web_service/request/request_param_test.rb
@@ -7,8 +7,8 @@ module WebService
         @value = 'true'
       end
 
-      def test_set_attributes
-        param = Exlibris::Primo::WebService::Request::RequestParam.new()
+      def test_set_attributes_for_xml
+        param = Exlibris::Primo::WebService::Request::RequestParam.new
         param.key = @key
         param.value = @value
         expected_xml = strip_xml(
@@ -20,7 +20,7 @@ module WebService
         assert_equal expected_xml, param.to_xml
       end
 
-      def test_write_attributes
+      def test_write_attributes_for_xml
         param = Exlibris::Primo::WebService::Request::RequestParam.new(:key => 'pc_availability_ind', :value => 'true')
         expected_xml = strip_xml(
           <<-XML
@@ -29,6 +29,37 @@ module WebService
         )
 
         assert_equal expected_xml, param.to_xml
+      end
+
+      def test_set_attributes_for_rest_with_known_key
+        param = Exlibris::Primo::WebService::Request::RequestParam.new
+        param.key = 'pcAvailability'
+        param.value = 'false'
+        expected_param = "pcAvailability=false"
+
+        assert_equal expected_param, param.to_s
+      end
+
+      def test_set_attributes_for_rest_with_unknown_key
+        param = Exlibris::Primo::WebService::Request::RequestParam.new
+        param.key = 'SomeKey'
+        param.value = 'true'
+
+        assert_raise_message(
+          "The provided request parameter key #{param.key} is unknown. " \
+          "Please provide one of the following: #{param.accepted.keys.join(',')}"
+        ) { param.to_s }
+      end
+
+      def test_set_attributes_for_rest_with_unacceptable_value
+        param = Exlibris::Primo::WebService::Request::RequestParam.new
+        param.key = 'pcAvailability'
+        param.value = 'T'
+
+        assert_raise_message(
+          "The provided request parameter value #{param.value} is unacceptable. " \
+          "Please provide one of the following: #{param.accepted[param.key].join(',')}"
+        ) { param.to_s }
       end
     end
   end
