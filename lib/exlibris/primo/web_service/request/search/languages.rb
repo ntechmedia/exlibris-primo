132f8ecbd280bcff4715b1e3322fa5231ce8430c
Michael Harrison <michael@ntechmedia.com>
2018-10-24 14:46:19 +1000

    Added Exlibris::Primo::WebService::Request::Languages.languages_string

--- lib/exlibris/primo/web_service/request/search/languages.rb
+++ lib/exlibris/primo/web_service/request/search/languages.rb
@@ -3,10 +3,10 @@ module Exlibris
     module WebService
       module Request
         module Languages
-          # 
+          #
           # Returns a lambda that takes a Nokogiri::XML::Builder as an argument
           # and appends languages XML to it.
-          # 
+          #
           def languages_xml
             lambda { |xml|
               xml.Languages {
@@ -17,6 +17,14 @@ module Exlibris
             }
           end
           protected :languages_xml
+
+          # Returns a string for inclusion in the as a query parameter for the REST API
+          def languages_string
+            return '' if languages.empty?
+
+            # TODO: Need to find out how to do multiple languages for the REST API.  For now we'll just use the first language
+            "lang=#{languages.first}"
+          end
 
           def languages
             @languages ||= []
--- test/web_service/request/languages_test.rb
+++ test/web_service/request/languages_test.rb
@@ -6,6 +6,10 @@ module WebService
         include Exlibris::Primo::WebService::Request::Languages
         include Exlibris::Primo::XmlUtil
 
+        def to_s
+          languages_string
+        end
+
         def to_xml
           build_xml { |xml|
             languages_xml.call xml
@@ -13,25 +17,43 @@ module WebService
         end
       end
 
-      def test_languages_xml_with_no_language_for_xml
+      def test_languages_xml_with_no_language
         search = SearchDummy.new
         expected_xml = ""
 
         assert_equal expected_xml, search.to_xml
       end
 
-      def test_languages_xml_with_a_language_for_xml
+      def test_languages_xml_with_multiple_languages
         search = SearchDummy.new
         search.add_language('eng')
+        search.add_language('fre')
         expected_xml = strip_xml(
           <<-XML
           <Languages>
             <Language>eng</Language>
+            <Language>fre</Language>
           </Languages>
         XML
         )
 
         assert_equal expected_xml, search.to_xml
+      end
+
+      def test_languages_string_with_no_language
+        search = SearchDummy.new
+        expected_output = ""
+
+        assert_equal expected_output, search.to_s
+      end
+
+      def test_languages_string_with_multiple_languages
+        search = SearchDummy.new
+        search.add_language('eng')
+        search.add_language('fre')
+        expected_output = 'lang=eng'
+
+        assert_equal expected_output, search.to_s
       end
     end
   end
