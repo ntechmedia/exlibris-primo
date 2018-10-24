cff52d6bc8f3e286a8f8975c48add8e11b8262b1
Michael Harrison <michael@ntechmedia.com>
2018-10-24 14:20:57 +1000

    Added tests for adaptors (even though we don't yet know how to use them for the REST API)

--- test/web_service/request/location_test.rb
+++ test/web_service/request/location_test.rb
@@ -3,27 +3,37 @@ module WebService
     require 'test_helper'
     class LocationTest < Test::Unit::TestCase
       def setup
-        @kind = "local"
-        @value = "scope:(VOLCANO)"
+        @kind_local = "local"
+        @value_local = "scope:(VOLCANO)"
+        @kind_adaptor = "adaptor"
+        @value_adaptor = "primo_central_multiple_fe"
       end
 
       def test_set_attributes
         location = Exlibris::Primo::WebService::Request::Location.new
-        location.kind = @kind
-        location.value = @value
+        location.kind = @kind_local
+        location.value = @value_local
         assert_equal "local", location.kind
         assert_equal "scope:(VOLCANO)", location.value
       end
 
       def test_write_attributes
-        location = Exlibris::Primo::WebService::Request::Location.new(:kind => @kind, :value => @value)
+        location = Exlibris::Primo::WebService::Request::Location.new(:kind => @kind_local, :value => @value_local)
         assert_equal "local", location.kind
         assert_equal "scope:(VOLCANO)", location.value
       end
 
-      def test_to_s
-        location = Exlibris::Primo::WebService::Request::Location.new(:kind => @kind, :value => @value)
+      def test_to_s_for_scopes
+        location = Exlibris::Primo::WebService::Request::Location.new(:kind => @kind_local, :value => @value_local)
         expected_output = 'VOLCANO'
+
+        assert_equal expected_output, location.to_s
+      end
+
+      def test_to_s_for_adaptors
+        # TODO: Find out how to include adaptors (e.g. primo_central_multiple_fe) for the REST
+        location = Exlibris::Primo::WebService::Request::Location.new(:kind => @kind_adaptor, :value => @value_adaptor)
+        expected_output = ''
 
         assert_equal expected_output, location.to_s
       end
