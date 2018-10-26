a09db6b90eb94657149f768304c112510f60e969
Michael Harrison <michael@ntechmedia.com>
2018-10-26 10:08:43 +1000

    Split Exlibris::Primo::WebService::Request::Call.call method into soap_call & rest_call

--- lib/exlibris/primo/web_service/request/base/call.rb
+++ lib/exlibris/primo/web_service/request/base/call.rb
@@ -3,14 +3,27 @@ module Exlibris
     module WebService
       module Request
         module Call
-          # 
+          #
           # Returns an response Exlibris::Primo::WebService::Response that corresponds
           # to the request.
-          # 
+          #
           def call
+            soap_call || rest_call
+          end
+
+          def soap_call
+            return unless (Exlibris::Primo.config.api || api) == :soap
+
             # Get the Response class that matches the Request class.
             response_klass = "Exlibris::Primo::WebService::Response::#{self.class.name.demodulize}".constantize
             response_klass.new(client.send(soap_action, to_xml), soap_action)
+          end
+
+          private :soap_call
+
+          def rest_call
+            return unless (Exlibris::Primo.config.api || api) == :rest
+
           end
         end
       end
