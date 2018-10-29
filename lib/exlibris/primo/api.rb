f65a346084b7b7478df8ffe8654887a80bc22d5a
Michael Harrison <michael@ntechmedia.com>
2018-10-30 09:56:23 +1000

    Fixed current_api method issue when the class is not including the Exlibris::Primo::Config::Attributes module

--- lib/exlibris/primo/api.rb
+++ lib/exlibris/primo/api.rb
@@ -2,7 +2,7 @@ module Exlibris
   module Primo
     module Api
       def current_api
-        api || Exlibris::Primo.config.api
+        methods.include?(:api) ? (api || Exlibris::Primo.config.api) : Exlibris::Primo.config.api
       end
     end
   end
