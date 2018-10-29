57bac99ea36f7f2c40e9cf2635f15204e9b1f9b6
Michael Harrison <michael@ntechmedia.com>
2018-10-29 11:48:24 +1100

    Added REST API Actions on Website::Client::Search

--- lib/exlibris/primo/web_service/client/search.rb
+++ lib/exlibris/primo/web_service/client/search.rb
@@ -8,6 +8,7 @@ module Exlibris
         class Search < Base
           self.endpoint = :searcher
           self.add_api_actions :soap, :search_brief, :get_record
+          self.add_api_actions :rest, :search
         end
       end
     end
