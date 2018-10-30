85416c4026d19f7e31a0405f71fd2df67fe35a61
Michael Harrison <michael@ntechmedia.com>
2018-10-30 11:22:28 +1000

    Added :soap as default for current_api when nothing is set

--- lib/exlibris/primo/api.rb
+++ lib/exlibris/primo/api.rb
@@ -2,7 +2,7 @@ module Exlibris
   module Primo
     module Api
       def current_api
-        methods.include?(:api) ? (api || Exlibris::Primo.config.api) : Exlibris::Primo.config.api
+        (methods.include?(:api) ? (api || Exlibris::Primo.config.api) : (Exlibris::Primo.config.api)) || :soap
       end
     end
   end
