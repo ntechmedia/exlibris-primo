5c19ef0498d93a8a02adb57cbdf918668dc47975
Michael Harrison <michael@ntechmedia.com>
2018-10-31 12:03:37 +1000

    Added test for Fulltext links

--- test/web_service/response/rest/records_test.rb
+++ test/web_service/response/rest/records_test.rb
@@ -31,7 +31,7 @@ module WebService
       end
 
       def test_holdings
-        VCR.use_cassette('rest/response/basic_search') do
+        VCR.use_cassette('rest/response/holdings') do
           api_action = :search
           request = Exlibris::Primo::WebService::Request::Search.new
           request.add_query_term 'Travels with My Aunt', "title", "contains"
@@ -42,7 +42,7 @@ module WebService
           assert_not_nil response.records
 
           holding = response.records.first.holdings.first
-
+          assert_not_nil holding
           assert_equal "UB_MILLENNIUM.b10019315", holding.record_id
           assert_equal "UB_MILLENNIUM.b10019315", holding.original_id
           assert_equal "Travels with my aunt", holding.title
@@ -58,6 +58,27 @@ module WebService
           assert(holding.eql?(holding))
           assert_equal(holding, holding.merge!(holding))
           assert(holding.expand.include?(holding))
+        end
+      end
+
+      def test_fulltexts
+        VCR.use_cassette('rest/response/fulltexts') do
+          api_action = :search
+          request = Exlibris::Primo::WebService::Request::Search.new
+          request.add_query_term 'Misbehavior online in higher education', "title", "contains"
+          request.add_location 'local', 'scope:(UB_local_PC)'
+          client = Exlibris::Primo::WebService::Client::Search.new(:base_url => @base_url)
+          response = Exlibris::Primo::WebService::Response::Search.new(client.send(api_action, request.query_params), api_action)
+
+          assert_not_nil response.records
+
+          link = response.records.first.fulltexts.first
+          assert_not_nil link
+          assert_nil link.institution
+          assert_equal "dedupmrg1317898213", link.record_id
+          assert_equal "UB_MILLENNIUM.b27288316", link.original_id
+          assert_equal "http://ezproxy.federation.edu.au/login?url=http://www.emeraldinsight.com/2044-9968/5", link.url
+          assert_equal "Access eBook online", link.display
         end
       end
     end
