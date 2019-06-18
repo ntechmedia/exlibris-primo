f2a24738bde61373365b33323afca4177b8b3e72
Michael Harrison <michael@ntechmedia.com>
2019-06-18 13:29:49 +1000

    Removed validation of facets as there are always more facets!

--- lib/exlibris/primo/web_service/request/search/facet.rb
+++ lib/exlibris/primo/web_service/request/search/facet.rb
@@ -10,13 +10,7 @@ module Exlibris
           include XmlUtil
           attr_accessor :category, :value
 
-          def initialize(*args)
-            validate_category(args[0][:category]) unless args.empty?
-            super
-          end
-
           def category=(value)
-            validate_category(value)
             @category = value
           end
 
@@ -24,26 +18,6 @@ module Exlibris
           # @param [String] operator the operator to use when rendering the facet to a string (default: exact)
           def to_s(operator: 'exact')
               "#{category},#{operator},#{URI.encode(value.gsub(';', ''))}"
-          end
-
-          # Validates the supplied category to ensure that it is supported
-          def validate_category(category)
-            return unless self.class.allowed_categories.exclude? category
-
-            raise "The supplied category '#{category}' is not supported. Please use one of the following: #{self.class.allowed_categories.join(', ')}"
-          end
-
-          def self.allowed_categories
-            [
-              "facet_rtype", # Resource Type
-              "facet_topic", # Subject
-              "facet_creator", # Author
-              "facet_tlevel", # Availability
-              "facet_domain", # Collection
-              "facet_library", # Library name
-              "facet_lang", # Language
-              "facet_lcc" # LCC classification
-            ]
           end
         end
       end
--- test/web_service/request/facet_test.rb
+++ test/web_service/request/facet_test.rb
@@ -19,30 +19,11 @@ module WebService
         assert_equal expected_param, facet.to_s
       end
 
-      def test_set_attributes_for_rest_with_unsupported_facet
-        facet = Exlibris::Primo::WebService::Request::Facet.new
-        expected_message =
-          "The supplied category 'facet_myfacet' is not supported. "\
-          "Please use one of the following: #{Exlibris::Primo::WebService::Request::Facet.allowed_categories.join(', ')}"
-
-        assert_raise_message(expected_message) {facet.category = 'facet_myfacet'}
-      end
-
       def test_write_attributes_for_rest
         facet = Exlibris::Primo::WebService::Request::Facet.new(value: 'books', category: 'facet_rtype')
         expected_param = "#{facet.category},exact,#{facet.value}"
 
         assert_equal expected_param, facet.to_s
-      end
-
-      def test_write_attributes_for_rest_with_unsupported_facet
-        expected_message =
-          "The supplied category 'facet_myfacet' is not supported. "\
-          "Please use one of the following: #{Exlibris::Primo::WebService::Request::Facet.allowed_categories.join(', ')}"
-
-        assert_raise_message(expected_message) do
-          Exlibris::Primo::WebService::Request::Facet.new(value: 'books', category: 'facet_myfacet')
-        end
       end
     end
   end
